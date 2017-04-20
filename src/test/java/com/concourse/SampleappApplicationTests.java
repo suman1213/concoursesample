d47f12c8a5c5575e090e724675ae2d1ee76a6366
pandey <suman.pandey@capgemini.com>
2017-04-20 20:15:41 +0530

    first commit for sample application

---
+++ src/test/java/com/concourse/SampleappApplicationTests.java
@@ -0,0 +1,21 @@
+package com.concourse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		//SampleappApplicationTests sampleappApplicationTests = new SampleappApplicationTests();
+		
+		 assertEquals(0,10*0);
+	}
+
+}
